ad24c351691321c152ae444fa38bb1e75ebf1b83
tuannguyen12 <tuan.nguyen12@sotatek.com>
2024-09-01 18:06:42 +0700

    feat: Add button to open sheet link

--- front-end/src/app/components/menu/menu.component.ts
+++ front-end/src/app/components/menu/menu.component.ts
@@ -41,4 +41,8 @@ export class MenuComponent {
   openRankings() {
     this.router.navigate(['/ranking']);
   }
+
+  openSheetLink() {
+    window.open('https://docs.google.com/spreadsheets/d/10ioKPL3Y4jofGZCCMJoFpFsl4tk5XBX-LuFN3XZSVKs/edit?usp=sharing', '_blank');
+  }
 }
